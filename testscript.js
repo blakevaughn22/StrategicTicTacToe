a7c2bd865b274a4b4cc098fe372cf586e2bc3783
Blake Vaughn <nfgwbv@gmail.com>
2022-01-07 14:26:53 -0500

    Added comments

--- testscript.js
+++ testscript.js
@@ -1,10 +1,12 @@
 // Author: Blake Vaughn
+// Rev 1.0
 
 const statusDisplay = document.querySelector('.game--status');
 
+// Allows the game to be active or not
 let gameActive = false;
 
-  
+// State of all of the cells and boards of the game
 let gameState = ["", "", "", "", "", "", "", "", "", "",
 				"", "", "", "", "", "", "", "", "", "",
 				"", "", "", "", "", "", "", "", "", "",
@@ -15,6 +17,7 @@ let gameState = ["", "", "", "", "", "", "", "", "", "",
 				"", "", "", "", "", "", "", "", "", "",
 				"", "", "", "", "", "", "", "", "", ""];
 				
+// Winning conditions for each board
 const miniWinningConditions = [
     [1, 2, 3],
     [4, 5, 6],
@@ -26,6 +29,7 @@ const miniWinningConditions = [
     [3, 5, 7]
 ];
 
+// Winning conditions for the game
 const winningConditions = [
     [0, 10, 20],
     [30, 40, 50],
@@ -37,27 +41,34 @@ const winningConditions = [
     [20, 40, 60]
 ];
 
+// Shows which boards can be played at any time
 let boardEnable = [true,true,true,true,true,true,true,true,true];
 
-let currentPlayer = "X";
+// Holds current player
+let currentPlayer = "X"; 
 
+// Different status messages for the game
 const winningMessage = () => `Player ${currentPlayer} has won!\nClick \"Start or Restart\" to play again!`;
 const drawMessage = () => `Game ended in a draw!\nClick \"Start or Restart\" to play again!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 const startMessage = () => `Click \"Start Game\" to begin!`;
+
+// Display the starting message
 statusDisplay.innerHTML = startMessage();
 
+// If a cell is played, make the cell display the current player 
 function handleCellPlayed(clickedCell, clickedCellIndex) {
     gameState[clickedCellIndex] = currentPlayer;
     clickedCell.innerHTML = currentPlayer;
-	//document.getElementById(clickedCellIndex.toString()).style.backgroundColor = "red"
 }
 
+// Changes the player between X and O
 function handlePlayerChange() {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
     statusDisplay.innerHTML = currentPlayerTurn();
 }
 
+// Determines which boards should be playable next turn and changes their color accordingly
 function handleBoardChange(clickedCellIndex) {
     for (let i = 0; i <= 80; i=i+10) {
 		if (i%10===0 && gameState[i]===''){
@@ -84,6 +95,7 @@ function handleBoardChange(clickedCellIndex) {
 	}
 }
 
+// Checks to see if a board has been won or drawn
 function checkMiniStatus(clickedCellIndex) {
 	let roundWon = false;
 	let board = clickedCellIndex-(clickedCellIndex%10);
@@ -124,6 +136,7 @@ function checkMiniStatus(clickedCellIndex) {
 	checkFullStatus(clickedCellIndex);
 }
 
+// Checks to see if the game has been won or drawn
 function checkFullStatus(clickedCellIndex) {
 	let roundWon = false;
     for (let i = 0; i <= 7; i++) {
@@ -162,6 +175,7 @@ function checkFullStatus(clickedCellIndex) {
 	handlePlayerChange();
 }
 
+// Determines whether the cell that was clicked is enabled and continues if it is
 function handleCellClick(clickedCellEvent) { 
     const clickedCell = clickedCellEvent.target;
 
@@ -178,6 +192,7 @@ function handleCellClick(clickedCellEvent) {
 
 }
 
+// When the start button is pressed, the game starts and default values are set
 function handleStartGame() {
 	if (!gameActive){
 		for (let j = 0; j<=80; j=j+10){
@@ -202,6 +217,7 @@ function handleStartGame() {
 		}
 }
 
+// When the restart button is pressed, the values are set to default and the game restarts
 function handleRestartGame() {
 	gameActive = true;
     currentPlayer = "X";
@@ -223,6 +239,7 @@ function handleRestartGame() {
                .forEach(cell => cell.innerHTML = "");
 }
 
+// Handles different actions by user
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
 document.querySelector('.game--start').addEventListener('click', handleStartGame);
 document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
